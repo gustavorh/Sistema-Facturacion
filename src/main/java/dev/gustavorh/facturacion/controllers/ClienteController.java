c7c32182705464cded90e05c1defe9345fd400c6
Gustavo Reyes <gustavo.reyes@tutanota.com>
2024-10-23 10:30:44 -0300

    Modified: Use of TH fragments.

--- src/main/java/dev/gustavorh/facturacion/controllers/ClienteController.java
+++ src/main/java/dev/gustavorh/facturacion/controllers/ClienteController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
@@ -40,39 +41,47 @@ public class ClienteController {
     }
 
     @PostMapping("/crear")
-    public String processCreate(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+    public String processCreate(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
         if (result.hasErrors()) {
             model.addAttribute("titulo", "Crear cliente");
             return "crear";
         }
         clienteService.save(cliente);
         status.setComplete();
+        flash.addFlashAttribute("success", "El cliente ha sido creado con éxito");
         return "redirect:/clientes";
     }
 
     @GetMapping("/editar/{id}")
     public String edit(@PathVariable(value = "id") Long id, Model model) {
-        Cliente cliente = clienteService.findById(id);
+        Cliente cliente = null;
+        if (id > 0) {
+            cliente = clienteService.findById(id);
+        } else {
+            return "redirect:/clientes";
+        }
         model.addAttribute("titulo", "Editar cliente");
         model.addAttribute("cliente", cliente);
         return "editar";
     }
 
     @PostMapping("/editar/{id}")
-    public String processEdit(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+    public String processEdit(@Valid Cliente cliente, BindingResult result, Model model, RedirectAttributes flash, SessionStatus status) {
         if (result.hasErrors()) {
             model.addAttribute("titulo", "Editar cliente");
             return "editar";
         }
         clienteService.save(cliente);
         status.setComplete();
+        flash.addFlashAttribute("success", "El cliente ha sido editado con éxito");
         return "redirect:/clientes";
     }
 
     @GetMapping("/eliminar/{id}")
-    public String delete(@PathVariable(value = "id") Long id) {
+    public String delete(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
         if (id > 0) {
             clienteService.delete(id);
+            flash.addFlashAttribute("success", "El cliente ha sido eliminado con éxito");
         }
         return "redirect:/clientes";
     }
